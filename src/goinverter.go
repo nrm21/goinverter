ffceb1962a39943001e3c0637fc135f21cd17254
nrm21 <natemorrison@gmail.com>
2023-02-23 17:19:36 -0500

    Major changes, removed pipes, restructured prog to continuous query and send results to webpage when ready, webpage now decoupled from query process.

--- src/goinverter.go
+++ src/goinverter.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -14,14 +15,12 @@ import (
 	"github.com/bearsh/hid"
 )
 
-var web2usbPipe chan string
 var usbResponse string
 var debug bool
-var lastUsbUpdate int64
+var lastUsbUpdate, usbUpdateInterval int64
 
 const httpIp = "" // leave blank for 0.0.0.0, else use a specific interface or localhost for more security
 const httpPort = "8088"
-const usbUpdateInterval int64 = 15
 
 // Make it possible to kill program by typing ctrl-C
 func SetupCloseHandler() {
@@ -29,17 +28,27 @@ func SetupCloseHandler() {
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		fmt.Println("\nCtrl+C pressed in Terminal\n")
+		fmt.Println("\nCtrl+C pressed in Terminal")
 		os.Exit(0)
 	}()
 }
 
-// Checks if the debug flag is set
-func checkDebugFlag() bool {
+// Checks for arguments to teh program on start
+func checkArgs() bool {
 	if len(os.Args) > 1 { // if we have arguments parse them
-		if os.Args[1] == "-d" {
-			fmt.Println("Debug flag set")
-			return true
+		arguments := os.Args
+		for i := 0; i < len(arguments); i++ {
+			if arguments[i] == "-d" {
+				fmt.Println("Debug flag set")
+				debug = true
+			} else if arguments[i] == "-i" {
+				i++
+				updateTime, err := strconv.Atoi(arguments[i])
+				usbUpdateInterval = int64(updateTime)
+				if err != nil {
+					fmt.Println("Error with interval argument parameter, check if it is a number")
+				}
+			}
 		}
 	}
 	return false
@@ -60,87 +69,85 @@ func handleHttpStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "status: \"ok\"\n")
 }
 
-// Sends QPIGS then QPIRI to the USB and returns the result of both
-func doStatusUpdate() string {
-	qr := &QueryResponse{}
-
-	cmd := "QPIGS"
-	web2usbPipe <- cmd
-	usbResponse = <-web2usbPipe
-	responseParser(cmd, &usbResponse, qr)
-
-	cmd = "QPIRI"
-	web2usbPipe <- cmd
-	usbResponse = <-web2usbPipe
-	responseParser(cmd, &usbResponse, qr)
-
-	// turn response into JSON and stringify it
-	jsonStruct, _ := json.Marshal(qr)
-
-	return string(jsonStruct)
-}
-
 // Handle http to run raw commands
 func handleHttpRaw(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	if strings.Contains(r.URL.Path, "query") {
-		if lastUsbUpdate+usbUpdateInterval < time.Now().Unix() {
-			usbResponse = doStatusUpdate()
-			//fmt.Fprintf(w, "%s\n", PrettyString(response)) // pretty print for webpage display
-			fmt.Fprintf(w, "%s\n", usbResponse)
-
-			lastUsbUpdate = time.Now().Unix()
-		} else { // we are querying again before the update interval expires, just return the previous data
-			if debug {
-				fmt.Println("Update interval not yet expired... resending previous data")
-			}
-			fmt.Fprintf(w, "%s\n", usbResponse)
-		}
+		fmt.Fprintf(w, "%s\n", PrettyString(usbResponse)) // pretty print for webpage display
+		//fmt.Fprintf(w, "%s\n", usbResponse)
 	} else if strings.Contains(r.URL.Path, "raw") {
 		// run command and send response to web
 		query := r.URL.Query()
-		web2usbPipe <- query.Get("cmd")
-		fmt.Fprintf(w, "%s\n", <-web2usbPipe) // and send response to console also
+		rawResponse := sendToInverterAndRetry(query.Get("cmd"))
+		fmt.Fprintf(w, "%s\n", rawResponse) // and send response to console also
 	}
 }
 
-// runs in a loop handling any misreads and doing them over again as needed
-func handleUSBTraffic() {
-	var devices []hid.DeviceInfo
-	devices = hid.Enumerate(0, 0)
-	inverterDev := devices[0] // the first device will usually be the one we want
+// Send command to inverter and retry if response was not correct
+func sendToInverterAndRetry(cmd string) string {
+	var response string
+	devices := hid.Enumerate(0, 0)
+	inverterDev := &devices[0] // the first device will usually be the one we want
 
+	if cmd == "QPIRI" || cmd == "QPIGS" {
+		var bytesRead int
+		for bytesRead < 104 {
+			response, bytesRead = writeToInverter(inverterDev, &cmd)
+			if debug && bytesRead < 104 {
+				fmt.Println("Less than 104 byte response... resending command to inverter")
+				time.Sleep(2500 * time.Millisecond)
+			}
+		}
+	} else { // else this isn't a query that returns a long response
+		response, _ = writeToInverter(inverterDev, &cmd)
+		//response = fmt.Sprintf("{\"msg\": \"%s\"}", response) // JSON-ify
+	}
+
+	return response
+}
+
+// Sends QPIGS then QPIRI to the USB and returns the result of both
+func doStatusUpdate() {
+	qr := &QueryResponse{}
+
+	cmd := "QPIGS"
+	response := sendToInverterAndRetry(cmd)
+	responseParser(cmd, &response, qr)
+
+	cmd = "QPIRI"
+	response = sendToInverterAndRetry(cmd)
+	responseParser(cmd, &response, qr)
+
+	// turn response into JSON and stringify it
+	jsonStruct, _ := json.Marshal(qr)
+
+	usbResponse = string(jsonStruct)
+	if debug {
+		fmt.Println("Status update complete\n")
+	}
+}
+
+// Runs in a loop handling any misreads and doing them over again as needed
+func handleUSBTraffic() {
 	// loop forever until user breaks since we are web serving
 	// and waiting for user response over and over again
 	for {
-		cmd := <-web2usbPipe
-
-		var response string
-		if cmd == "QPIRI" || cmd == "QPIGS" {
-			var bytesRead int
-			for bytesRead < 104 {
-				response, bytesRead = writeToInverter(&inverterDev, &cmd)
-				if debug && bytesRead < 104 {
-					fmt.Println("Less than 104 byte response... resending command to inverter")
-					time.Sleep(2500 * time.Millisecond)
-				}
-			}
-		} else { // else this isn't a query that returns a long response
-			response, _ = writeToInverter(&inverterDev, &cmd)
-			//response = fmt.Sprintf("{\"msg\": \"%s\"}", response) // JSON-ify
+		if lastUsbUpdate+usbUpdateInterval < time.Now().Unix() {
+			doStatusUpdate() // saves data to usbResponse var
+			lastUsbUpdate = time.Now().Unix()
+		} else {
+			time.Sleep(1 * time.Second)
 		}
-
-		web2usbPipe <- response
 	}
 }
 
 func main() {
-	SetupCloseHandler()
-	debug = checkDebugFlag()
+	// set defaults incase not set by program
+	usbUpdateInterval = 10
 
-	// initiate our globally declared channels here
-	web2usbPipe = make(chan string)
+	SetupCloseHandler()
+	checkArgs()
 
 	http.HandleFunc("/status", handleHttpStatus) // health check (for k8s readiness probes)
 	http.HandleFunc("/health", handleHttpStatus) // health check (for k8s readiness probes)
